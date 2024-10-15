50cfd232cefe3cc1795c36bb4471dd3a804962b5
Sumit Kumar Sourabh <sumitsourabh1993@gmail.com>
2024-10-15 11:49:26 +0530

    add dagshub auth

--- src/model/model_evalution.py
+++ src/model/model_evalution.py
@@ -129,8 +129,23 @@ import mlflow.sklearn
 from mlflow.models import infer_signature
 
 # Set the tracking URI to DAGsHub
-mlflow.set_tracking_uri('https://dagshub.com/Sumitdatascience/Churn_model_development.mlflow')
-dagshub.init(repo_owner='Sumitdatascience', repo_name='Churn_model_development', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/Sumitdatascience/Churn_model_development.mlflow')
+# dagshub.init(repo_owner='Sumitdatascience', repo_name='Churn_model_development', mlflow=True)
+
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "Sumitdatascience"
+repo_name = "Churn_model_development"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 
 # Load the trained model
