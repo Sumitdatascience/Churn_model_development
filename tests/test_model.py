a5ee6e588b1f006841097a3aab580a1fbd322421
Sumit Kumar Sourabh <sumitsourabh1993@gmail.com>
2024-10-20 13:24:19 +0530

    add test model signature

--- tests/test_model.py
+++ tests/test_model.py
@@ -52,6 +52,7 @@ import os
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
+import numpy as np
 
 class TestModelLoading(unittest.TestCase):
 
@@ -85,7 +86,36 @@ class TestModelLoading(unittest.TestCase):
         return latest_version[0].version if latest_version else None
 
     def test_model_loaded_properly(self):
-        self.assertIsNotNone(self.new_model)    
+        self.assertIsNotNone(self.new_model)   
+
+
+
+    def test_model_signature(self):
+        # Define the data and columns
+        data = [12, 'desktop', 1, 2, 'credit card', 'male', 2, 3, 'fashion', 2, 0, 3.0, 0, 13.0, 10, 20.0, 160.0]
+        columns = ['Tenure', 'PreferredLoginDevice', 'CityTier', 'WarehouseToHome', 'PreferredPaymentMode', 'Gender',
+                   'HourSpendOnApp', 'NumberOfDeviceRegistered', 'PreferredOrderCat', 'SatisfactionScore',
+                   'MaritalStatus', 'NumberOfAddress', 'Complain', 'OrderAmountHikeFromLastYear', 'OrderCount',
+                   'DaySinceLastOrder', 'CashbackAmount']
+
+        # Create a pandas DataFrame using the data and columns
+        input_df = pd.DataFrame([data], columns=columns)
+
+        # Predict using the new churn prediction model
+        prediction = self.new_model.predict(input_df)
+
+        # Verify that the input shape matches the number of columns expected by the model
+        self.assertEqual(input_df.shape[1], len(columns))
+
+        # Verify the output shape (assuming binary classification with a single output per row)
+        self.assertEqual(len(prediction), input_df.shape[0])
+
+        # Assuming the prediction is a 1D array of binary values (0 or 1 for churn prediction)
+        self.assertEqual(len(prediction.shape), 1)
+
+        # Check that the predictions are binary (either 0 or 1)
+        unique_values = np.unique(prediction)
+        self.assertTrue(set(unique_values).issubset({0, 1}))     
 
 if __name__ == '__main__':
     unittest.main()
